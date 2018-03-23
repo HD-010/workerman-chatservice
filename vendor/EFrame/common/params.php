a688f52e9105a55e3fe3a1c422a155e79a1e8beb
root <912744609@qq.com>
2018-03-23 15:03:17 +0800

    dd

--- vendor/EFrame/common/params.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-        '@view' => dirname(__DIR__).'\View.php',
-        '@control' => dirname(__DIR__).'\Control.php',
-        '@model' => dirname(__DIR__).'\Model.php',
-        '@block' => dirname(__DIR__).'\Block.php',
-];
