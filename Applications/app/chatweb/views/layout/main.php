3c8392f7f7ddfe419d941403d8291fd60003d37d
HD-010 <912744609@qq.com>
2018-04-18 09:56:08 +0800

    修复页面已知错误

--- Applications/app/chatweb/views/layout/main.php
+++ Applications/app/chatweb/views/layout/main.php
@@ -3,14 +3,14 @@
 		<meta charset="utf-8">
 		<title>Workerman小蝌蚪互动聊天室 HTML5+Websocket+PHP多进程socket实时推送技术</title>
 		<link rel="stylesheet" type="text/css" href="/css/mainService.css" />
-		<meta name="viewport" content="width=device-width initial-scale=1.0 maximum-scale=1.0  minimum-scale=1.0  user-scalable=0;" />		
+		<meta name="viewport" content="width=device-width initial-scale=1.0 maximum-scale=1.0  minimum-scale=1.0  user-scalable=0" />		
     <meta name="apple-mobile-web-app-capable" content="YES">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
-	   <link rel="apple-touch-icon" href="/images/apple-touch-icon.png"/>
+	   <link rel="apple-touch-icon" href=""/>
 		<meta property="fb:app_id" content="149260988448984" />
 		<meta name="title" content="Workerman-todpole!" />
 		<meta name="description" content="workerman + HTML5+WebSocket +PHP socket 广播 小蝌蚪交互游戏程序 ，坐标实时推送、实时聊天等" />
-		<link rel="image_src" href="/images/fb-image.jpg" / >
+		<link rel="image_src" href="" / >
 	</head>
 	<body>
 		<script type="text/javascript" data-main="/js/mainService" src="http://cdn.e01.ren/common/require/require.js"></script>
