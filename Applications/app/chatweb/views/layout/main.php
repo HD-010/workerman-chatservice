7bd2ec5fe3205f95f798d782daeca5cfa4e11726
HD-010 <912744609@qq.com>
2018-04-23 19:01:42 +0800

    修改布局文件中js文件引入地址为远程地址

--- Applications/app/chatweb/views/layout/main.php
+++ Applications/app/chatweb/views/layout/main.php
@@ -13,7 +13,7 @@
 		<link rel="image_src" href="" / >
 	</head>
 	<body>
-		<script type="text/javascript" data-main="/js/mainService" src="http://cdn.e01.ren/common/require/require.js"></script>
+		<script type="text/javascript" data-main="/js/mainService" src="http://cdn.sqc666.com/common/require/require.js"></script>
 	
         <?php
             $this->contents();
