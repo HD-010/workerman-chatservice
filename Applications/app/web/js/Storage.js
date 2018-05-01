ffaaa148cf0c4427241c2eed76cc7b3de932f28f
HD-010 <912744609@qq.com>
2018-05-01 18:24:34 +0800

    debug

--- Applications/app/web/js/Storage.js
+++ Applications/app/web/js/Storage.js
@@ -2,38 +2,39 @@
  * 为兼容低版浏览器，使用localStorage存储
  */
 define(['common'],function(){
-	var ST = function(){}
-		localStorage = {
-			setItem:function(key,value){
-				common.setCookie(key,value,360)
-			        return true;	
-			},
-			getItem:function(key){
-				return common.getCookies(key)
-			}
-		};
-		
-		st.sessionStorage = {
-			setItem:function(key,value){
-                                common.setCookie(key,value,1/24)
-                                return true;
-                        },
-                        getItem:function(key){
-                                return common.getCookies(key)
-                        }
-
-		};
-		
-		
+	var localStorage = {
+		setItem:function(key,value){
+			common.setCookie(key,value,360)
+		        return true;	
+		},
+		getItem:function(key){
+			return common.getCookies(key)
+		}
 	};
+		
+	var sessionStorage = {
+		setItem:function(key,value){
+            common.setCookie(key,value,1/24)
+            return true;
+        },
+        getItem:function(key){
+            return common.getCookies(key)
+        }
+
+	};
+	
+	var ST = {
+		localStorage:localStorage,
+		sessionStorage:sessionStorage
+	};
+		
 
 	
 	(function(){
 		if(typeof(Storage) === 'undefined'){
-			var st = new ST();
-			window.Storage = 'st';
-			//window.localStorage = st.localStorage;
-			//window.sessionStorage = st.sessionStorage;
+			window.Storage = ST;
+			window.localStorage = localStorage;
+			window.sessionStorage = sessionStorage;
 		}
 	})()
 });
