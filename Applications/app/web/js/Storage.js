c45324f17c5081f1d2ecf147c8dc616e91cc2e2b
HD-010 <912744609@qq.com>
2018-05-01 18:32:25 +0800

    debug

--- Applications/app/web/js/Storage.js
+++ Applications/app/web/js/Storage.js
@@ -2,39 +2,33 @@
  * 为兼容低版浏览器，使用localStorage存储
  */
 define(['common'],function(){
-	var localStorage = {
-		setItem:function(key,value){
-			common.setCookie(key,value,360)
-		        return true;	
-		},
-		getItem:function(key){
-			return common.getCookies(key)
-		}
-	};
-		
-	var sessionStorage = {
-		setItem:function(key,value){
-            common.setCookie(key,value,1/24)
-            return true;
-        },
-        getItem:function(key){
-            return common.getCookies(key)
-        }
-
-	};
-	
-	var ST = {
-		localStorage:localStorage,
-		sessionStorage:sessionStorage
-	};
-		
-
-	
 	(function(){
 		if(typeof(Storage) === 'undefined'){
-			window.Storage = ST;
-			window.localStorage = localStorage;
-			window.sessionStorage = sessionStorage;
+			window.localStorage = {
+				setItem:function(key,value){
+					common.setCookie(key,value,360)
+				        return true;	
+				},
+				getItem:function(key){
+					return common.getCookies(key)
+				}
+			};
+			
+			window.sessionStorage = {
+				setItem:function(key,value){
+		            common.setCookie(key,value,1/24)
+		            return true;
+		        },
+		        getItem:function(key){
+		            return common.getCookies(key)
+		        }
+
+			};
+			
+			window.Storage = {
+				sessionStorage:window.sessionStorage,
+				localStorage:window.localStorage
+			};
 		}
 	})()
 });
